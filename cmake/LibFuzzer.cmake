69e851eac3f75215d4983d6a2f01cec4b9db2703
Template Janitor <template.janitor@example.com>
2023-06-21 21:55:48 +0000

    Cleanup template and initialize repository

--- cmake/LibFuzzer.cmake
+++ cmake/LibFuzzer.cmake
@@ -1,4 +1,4 @@
-function(myproject_check_libfuzzer_support var_name)
+function(test0_check_libfuzzer_support var_name)
   set(LibFuzzerTestSource
       "
 #include <cstdint>
--- src/sample_library/sample_library.cpp
+++ src/sample_library/sample_library.cpp
@@ -1,4 +1,4 @@
-#include <myproject/sample_library.hpp>
+#include <test0/sample_library.hpp>
 
 int factorial(int input) noexcept
 {
